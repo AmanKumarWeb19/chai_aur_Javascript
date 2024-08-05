7b74a72fe23610e31aaff12382574c0222cf4337
Aman Kumar <akgupta0524@gmail.com>
2024-08-05 16:40:18 +0530

    converstion

--- basic-1/03_conversionAndOperation.js
+++ basic-1/03_conversionAndOperation.js
@@ -2,21 +2,45 @@
 
 let score = true;
 
-console.log(typeof score);
-console.log(typeof score);
+// console.log(typeof score);
 
 let valueInNumber = String(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 let someNumber = 33;
 let stringNumber = Boolean(someNumber);
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
 
 let res = 0 / 0;
-console.log(res);
+// console.log(res);
 
-console.log(Number.isNaN(res)); // checking the value is NaN;
+// console.log(Number.isNaN(res)); // checking the value is NaN;
 
 // ***************************************Operation************************************************************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 + 2);
+// console.log(2 / 5);
+// console.log(2 ** 2);
+// console.log(2 % 5);
+
+let str1 = "Aman";
+let str2 = " Kumar";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2 + 2);
+// console.log(2 + 2 + "1");
+// console.log("1" + 2);
+// console.log(1 + "2");
+
+console.log(+true);
+console.log(+"");
