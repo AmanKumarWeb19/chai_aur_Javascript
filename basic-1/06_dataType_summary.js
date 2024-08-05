914440880d8d3d72ca8db64a7ee0bd4e8e0aa2e1
Aman Kumar <akgupta0524@gmail.com>
2024-08-05 17:39:36 +0530

    summery datatypes

---
+++ basic-1/06_dataType_summary.js
@@ -0,0 +1,29 @@
+/**
+ * premitive data Type:-
+ * a. Number
+ * b. String
+ * c. Bolean
+ * d. Null
+ * e. undefined
+ * f. Symbol
+ * g. BigInt
+ *
+ * Non-Premitive DataType(Reference DataType):-
+ *
+ * a. Array
+ * b. Object
+ * c. Function
+ *
+ */
+
+const id = "123";
+const id2 = 123;
+// console.log(id == id2);
+// console.log(id === id2);
+
+const x = Symbol("124");
+const x2 = Symbol("124");
+// console.log(x === x2);
+// console.log(x == x2);
+
+const bigNumber = 1234543214567532244n;
