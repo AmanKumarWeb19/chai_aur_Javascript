5803b29dfc712570fc88647aa142430060c1cf29
Aman Kumar <akgupta0524@gmail.com>
2024-08-05 14:06:51 +0530

    datatypes

---
+++ basic-1/01_variables.js
@@ -0,0 +1,22 @@
+ const accountId = 12356578
+let accountEmail = "ak@gmail.com"
+var accountPassword = "123456"
+accountCity = "Patna"
+
+// accountId = 2 // not allowed
+
+accountEmail = "rk@gmail.com"
+
+accountPassword = "wreyia"
+
+accountCity = "mumbai"
+
+let accountState
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/** 
+ * Prefer not used to Var..
+ */
---
+++ basic-1/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all the js code as newer version.
+
+// alert(3+3) //We are using  nodeJs., not browser
+
+console.log(3 + 3); // code readability should be high
+
+/** Primitive DataTypes :-
+ *
+ * a. Number
+ * b. bigint
+ * c. String
+ * d. boolean
+ * e. null
+ * f. undefined
+ * g. symbol
+ *
+ *
+ *  Non-Primitive DataTypes:-
+ *
+ * a. Objects
+ */
