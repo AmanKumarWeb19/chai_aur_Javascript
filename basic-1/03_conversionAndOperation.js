cc265c56d0617856cb63fd9fa2ccf28da8192f51
Aman Kumar <akgupta0524@gmail.com>
2024-08-05 14:48:18 +0530

    check isNaN

--- basic-1/03_conversionAndOperation.js
+++ basic-1/03_conversionAndOperation.js
@@ -11,3 +11,8 @@ let someNumber = 33;
 let stringNumber = Boolean(someNumber);
 console.log(typeof stringNumber);
 console.log(stringNumber);
+
+let res = 0 / 0;
+console.log(res);
+
+console.log(Number.isNaN(res)); // checking the value is NaN;
