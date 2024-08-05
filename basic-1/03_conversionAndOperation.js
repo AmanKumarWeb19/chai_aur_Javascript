21a5ec78339b82111bd9e237a6c626a3c4e1ca48
Aman Kumar <akgupta0524@gmail.com>
2024-08-05 14:39:30 +0530

    conversionandoperation

---
+++ basic-1/03_conversionAndOperation.js
@@ -0,0 +1,13 @@
+let score = true;
+
+console.log(typeof score);
+console.log(typeof score);
+
+let valueInNumber = String(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let someNumber = 33;
+let stringNumber = Boolean(someNumber);
+console.log(typeof stringNumber);
+console.log(stringNumber);
