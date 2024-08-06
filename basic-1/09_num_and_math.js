0e489dd2faed490ccc89bcadf82c7bf5799330cb
Aman Kumar <akgupta0524@gmail.com>
2024-08-06 21:03:12 +0530

    num and math

---
+++ basic-1/09_num_and_math.js
@@ -0,0 +1,39 @@
+const score = 100;
+// console.log(score);
+
+const balance = new Number(400);
+// console.log(balance);
+
+// console.log(balance.toString());
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.5647;
+// console.log(otherNumber.toString.length);
+// console.log(otherNumber.toPrecision(4));
+
+const hundred = 1000000;
+
+// console.log(hundred.toLocaleString("en-IN"));
+
+/** ***********************************************  MATH **************************************************** */
+
+// console.log(Math);
+
+// console.log(Math.abs(-7));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.9));
+// console.log(Math.sqrt(16));
+// console.log(Math.max(16, 2, 15, 18, 15));
+// console.log(Math.min(16, 2, 15, 18, 15));
+// console.log(Math.random());
+
+// console.log(Math.random() * 10 + 1);
+// console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
