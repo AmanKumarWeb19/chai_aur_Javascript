edbb43c428c8528b2f4252eee7772deeb5ad2998
Aman Kumar <akgupta0524@gmail.com>
2024-08-06 15:39:16 +0530

    string and their method

---
+++ basic-1/08_String.js
@@ -0,0 +1,27 @@
+const name = "Aman";
+const repoCount = 70;
+
+// console.log(`Hello My name is ${name} and my repo count is ${repoCount}`);
+
+const strGame = new String("     Amankumar-hcrrt    ");
+// console.log(strGame);
+// console.log(strGame.__proto__);
+
+// console.log(strGame.length);
+// console.log(strGame.toUpperCase());
+// console.log(strGame.toLowerCase());
+// console.log(strGame.charAt(2));
+// console.log(strGame.indexOf("k"));
+
+const newString = strGame.substring(-4, 8);
+// console.log(newString)
+const strSlice = strGame.slice(5, 12);
+// console.log(strSlice);
+
+const txt = "   suhss   ";
+// console.log(txt);
+// console.log(txt.trim());
+
+let text = "Please visit Microsoft!";
+let newText = text.replace("Microsoft", "W3Schools");
+console.log(newText);
