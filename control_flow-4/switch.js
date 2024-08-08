727c5b0c84518c6945f9a8b0ec742b01c42be9a3
Aman Kumar <akgupta0524@gmail.com>
2024-08-08 17:20:23 +0530

    complete control flow

---
+++ control_flow-4/switch.js
@@ -0,0 +1,47 @@
+// const month = 3;
+// switch (month) {
+//   case 1:
+//     console.log("January");
+//     break;
+//   case 2:
+//     console.log("feb");
+//     break;
+//   case 3:
+//     console.log("march");
+//     break;
+//   case 4:
+//     console.log("April");
+//     break;
+
+//   default:
+//     console.log("default case match");
+
+//     break;
+// }
+
+const day = "thrusday";
+switch (day) {
+  case "monday":
+    console.log("monday");
+    break;
+  case "tuesday":
+    console.log("tuesday");
+    break;
+  case "wednesday":
+    console.log("wednesday");
+    break;
+  case "thrusday":
+    console.log("thrusday");
+    break;
+  case "friday":
+    console.log("friday");
+    break;
+  case "saturday":
+    console.log("saturday");
+    break;
+
+  default:
+    console.log("correct day find");
+
+    break;
+}
---
+++ control_flow-4/truthyorfalsy.js
@@ -0,0 +1,41 @@
+// const userEmail = "al@gmail.com";
+// if (userEmail) {
+//   console.log("login userEmail");
+// } else {
+//   console.log("Don't have user Email");
+// }
+
+//
+
+// to Check Array is Empty or Not.
+
+// const arr = [];
+
+// if (arr.length === 0) {
+//   console.log("Array is Empty");
+// }
+
+// // to Check Object is Empty or Not.
+
+// const obj = {};
+
+// if (Object.keys(obj).length === 0) {
+//   console.log("Object is Empty");
+// }
+
+// Nullish Coalescing Operator (??): null and Undefined
+
+let val1;
+// val1 = 5 ?? 10; // output - 5
+// val1 = null ?? 10; // output - 10
+// val1 = undefined ?? 10; // output - 10
+// val1 = null ?? 23 ?? 34; // output - 23
+// console.log(val1);
+
+// Terniary Operator:-
+
+const iceTeaPrice = 100;
+
+iceTeaPrice >= 80
+  ? console.log("greater than 80")
+  : console.log("less than 80");
