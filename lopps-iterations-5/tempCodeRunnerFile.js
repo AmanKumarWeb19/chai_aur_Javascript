8c7c56c1dc3c627681a453d7bf26a8bf929923c1
Aman Kumar <akgupta0524@gmail.com>
2024-08-08 19:20:19 +0530

    while loop and do while loop

---
+++ lopps-iterations-5/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+let index = 1;
+
+// while (index <= 10) {
+//   console.log(`In While Loop value of index is ${index}`);
+//   index = index + 2;
+// }
---
+++ lopps-iterations-5/two.js
@@ -0,0 +1,23 @@
+// let index = 1;
+
+// while (index <= 10) {
+//   console.log(`In While Loop value of index is ${index}`);
+//   index = index + 2;
+// }
+
+// const myArray = ["Batman", "Flash", "SuperMan"];
+// let arr = 0;
+
+// while (arr < myArray.length) {
+//   console.log(`My superHero Are present in array ${myArray[arr]}`);
+//   arr++;
+// }
+
+/** ----------------------------------------------DO WHILE LOOP--------------------------------------------- */
+
+let score = 1;
+
+do {
+  console.log(`value is ${score}`);
+  score++;
+} while (score <= 10);
