4f7254175afd2f2ac0b10c8e999013af10ff3f10
Aman Kumar <akgupta0524@gmail.com>
2024-08-14 11:10:06 +0530

    modify my clasess.js

--- 10_Opps-Classes/myClasses.js
+++ 10_Opps-Classes/myClasses.js
@@ -1,15 +1,40 @@
 // .......................... ........... ......ES6 ............... ...................... ...................
 
-class User {
-  constructor(username, email, pass) {
-    this.username = username;
-    this.email = email;
-    this.pass = pass;
-  }
-  encryptPassword() {
-    return `${this.pass}abc`;
-  }
+// class User {
+//   constructor(username, email, pass) {
+//     this.username = username;
+//     this.email = email;
+//     this.pass = pass;
+//   }
+//   encryptPassword() {
+//     return `${this.pass}abc`;
+//   }
+
+//   changeUserName() {
+//     return `${this.username.toUpperCase()}`;
+//   }
+// }
+
+// const chai = new User("aman", "chai@hmail.com", "123");
+// console.log(chai.encryptPassword());
+// console.log(chai.changeUserName());
+
+// Behind the Scene:-
+
+function User(username, email, pass) {
+  this.username = username;
+  this.email = email;
+  this.pass = pass;
 }
 
-const chai = new User("aman", "chai@hmail.com", "123");
-console.log(chai.encryptPassword());
+User.prototype.encryptPassword = function () {
+  return `${this.pass}abxc`;
+};
+
+User.prototype.changeUserName = function () {
+  return `${this.username.toUpperCase()}`;
+};
+
+const tea = new User("tea", "tea@hmail.com", "123");
+console.log(tea.encryptPassword());
+console.log(tea.changeUserName());
